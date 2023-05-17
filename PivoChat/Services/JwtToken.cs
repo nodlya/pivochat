82f4735f38d4c4694c882f9ee6e3c6f3a13e7b6f
Seraphic <danilsocol-2002@mail.ru>
2023-05-17 21:22:17 +0500

    fix

--- PivoChat/Services/JwtToken.cs
+++ PivoChat/Services/JwtToken.cs
@@ -10,21 +10,25 @@ public static class JwtToken
 {
     public static string GenerateToken(User user)
     {
-        var claims = new List<Claim>
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes("very_secret_and_complex_key_12345");
+      
+        var tokenDescriptor = new SecurityTokenDescriptor
         {
-            new Claim(ClaimTypes.Name, user.Login),
-            new Claim(ClaimTypes.Name, user.Name)
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Login),
+                new Claim(ClaimTypes.Name, user.Name)
+            }),
+            Expires = DateTime.UtcNow.AddHours(24),
+            Issuer = "AuthOptions.ISSUER",
+            Audience = "AuthOptions.AUDIENCE",
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
         };
-        var jwt = new JwtSecurityToken(
-            issuer: "AuthOptions.ISSUER",
-            audience: "AuthOptions.AUDIENCE",
-            claims: claims,
-            expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)), // время действия 2 минуты
-            signingCredentials: new SigningCredentials(
-                new SymmetricSecurityKey(Encoding.UTF8.GetBytes("very_secret_and_complex_key_12345")), 
-                SecurityAlgorithms.HmacSha256));
-            
-        return new JwtSecurityTokenHandler().WriteToken(jwt);
+      
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
+      
     }
 
 }
